7b605c2b05154b5abeb0c1c97e2f7a5bae802bbf
D <D>
2024-12-05 10:53:51 +0000

    Change brackets

--- src/ghosts.pandora/app/tests/api/test_doc.py
+++ src/ghosts.pandora/app/tests/api/test_doc.py
@@ -38,8 +38,9 @@ def test_return_doc_with_invalid_filename(method):
     response = getattr(client, method)(f"/doc/{invalid_file_name}")
     assert response.status_code == 400
     assert response.json() == {
-        "detail': 'Invalid file extension. Allowed extensions are: .doc, .docx, .dot, .dotx, .docm, .dotm, .odt"
+        "detail": "Invalid file extension. Allowed extensions are: .doc, .docx, .dot, .dotx, .docm, .dotm, .odt"
     }
+
 
 
 @pytest.mark.parametrize("method", ["get", "post"])
